ccbf3ee6b7812aa05bc78ef53f99df59959d667e
rafaafonseca <rafelabraga017@gmail.com>
2026-05-05 10:59:36 -0300

    Alterações
    Alterado título e incluída função de concluirTarefa. (Feita pelo membro 3)

--- tarefa.js
+++ tarefa.js
@@ -1,6 +1,6 @@
 // tarefas.js - Sistema de Lista de Tarefas
 let tarefas = [];
-let titulo = "GERENCIADOR DE TAREFAS"; // <- linha do conflito
+let titulo = "LISTA DE TAREFAS V2"; // <- linha do conflito
 function adicionarTarefa(descricao) {
   tarefas.push({ descricao: descricao, concluida: false });
   console.log("Tarefa adicionada: " + descricao);
@@ -29,3 +29,13 @@ console.log("Removida: " + rem[0].descricao);
 // Teste
 adicionarTarefa("Tarefa B");
 removerTarefa(1); listarTarefas();
+function concluirTarefa(indice) {
+if (indice < 1 || indice > tarefas.length) {
+console.log("Indice invalido."); return;
+}
+tarefas[indice - 1].concluida = true;
+console.log("Concluida: " + tarefas[indice-1].descricao);
+}
+// Teste
+adicionarTarefa("Tarefa C");
+concluirTarefa(1); listarTarefas();
